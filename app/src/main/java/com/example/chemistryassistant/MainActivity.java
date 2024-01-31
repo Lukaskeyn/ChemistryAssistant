d150f108f8285e99323087e1a3e142ed64be11b2
lukke <lukkeyn33@gmail.com>
2024-02-01 00:16:19 +0300

    Start page: create logo

--- app/src/main/java/com/example/chemistryassistant/MainActivity.java
+++ app/src/main/java/com/example/chemistryassistant/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.chemistryassistant;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     ImageView normalModeButton, chemistryModeButton;
