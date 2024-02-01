a9f4ff44c8d6cdeaf346cef4a844c7db83ff91d8
lukke <lukkeyn33@gmail.com>
2024-02-01 23:33:12 +0300

    Normal_mode page: base arithmetics

--- app/src/main/java/com/example/chemistryassistant/NormalMode.java
+++ app/src/main/java/com/example/chemistryassistant/NormalMode.java
@@ -8,9 +8,18 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class NormalMode extends AppCompatActivity {
     private TextView resultTextView;
+    private TextView operations;
+    private TextView latestNumber;
     private Button backOnPreviousPage;
+    boolean mathFlag = false;
+    boolean equalPush = false;
+
+    private int result = 0;
+
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -18,6 +27,8 @@ public class NormalMode extends AppCompatActivity {
         setContentView(R.layout.normal_mode_page);
 
         resultTextView = findViewById(R.id.ResultView);
+        operations = findViewById(R.id.operationsView);
+        latestNumber = findViewById(R.id.textView2);
 
         backOnPreviousPage = findViewById(R.id.BackPageNButton);
         backOnPreviousPage.setOnClickListener(v -> {
@@ -26,30 +37,101 @@ public class NormalMode extends AppCompatActivity {
         } );
     }
     public void clickNumber(View view) {
-        String number = resultTextView.getText().toString();
-        if (view.getId() == R.id.button0)
-            number = number + "0";
-        else if (view.getId() == R.id.button1)
-            number = number + "1";
-        else if (view.getId() == R.id.button2)
-            number = number + "2";
-        else if (view.getId() == R.id.button3)
-            number = number + "3";
-        else if (view.getId() == R.id.button4)
-            number = number + "4";
-        else if (view.getId() == R.id.button5)
-            number = number + "5";
-        else if (view.getId() == R.id.button6)
-            number = number + "6";
-        else if (view.getId() == R.id.button7)
-            number = number + "7";
-        else  if (view.getId() == R.id.button8)
-            number = number + "8";
-        else if (view.getId() == R.id.button9)
-            number = number + "9";
-        else number = "Что-то пошло не так";
+        String number;
+        if (equalPush) {
+            resultTextView.setText("");
+            equalPush = false;
+        }
+        if (mathFlag) {
+            resultTextView.setText("");
+            mathFlag = false;
+        }
+            if (!mathFlag && !equalPush) {
+                number = resultTextView.getText().toString();
+                if (view.getId() == R.id.button0)
+                    number = number + "0";
+                else if (view.getId() == R.id.button1)
+                    number = number + "1";
+                else if (view.getId() == R.id.button2)
+                    number = number + "2";
+                else if (view.getId() == R.id.button3)
+                    number = number + "3";
+                else if (view.getId() == R.id.button4)
+                    number = number + "4";
+                else if (view.getId() == R.id.button5)
+                    number = number + "5";
+                else if (view.getId() == R.id.button6)
+                    number = number + "6";
+                else if (view.getId() == R.id.button7)
+                    number = number + "7";
+                else if (view.getId() == R.id.button8)
+                    number = number + "8";
+                else if (view.getId() == R.id.button9)
+                    number = number + "9";
 
-        resultTextView.setText(number);
-    }
+
+                resultTextView.setText(number);
+            }
+        }
+
+
+  public void mathematics(View view) {
+      if (resultTextView.getText().length() > 0) {
+          mathFlag = true;
+          if (view.getId() == R.id.buttonPlus) {
+              operations.setText("+");
+              latestNumber.setText(resultTextView.getText());
+              result = Integer.parseInt(latestNumber.getText().toString());
+          }
+          else if (view.getId() == R.id.buttonMinus) {
+              operations.setText("-");
+              latestNumber.setText(resultTextView.getText());
+              result = Integer.parseInt(latestNumber.getText().toString());
+          }
+          else if (view.getId() == R.id.buttonMultiply) {
+              operations.setText("*");
+              latestNumber.setText(resultTextView.getText());
+              result = Integer.parseInt(latestNumber.getText().toString()) ;
+          }
+          else if (view.getId() == R.id.buttonDivide) {
+              operations.setText("/");
+              latestNumber.setText(resultTextView.getText());
+              result = Integer.parseInt(latestNumber.getText().toString()) ;
+          }
+
+      }
+  }
+  public void showResult(View view) {
+        if(view.getId() == R.id.buttonEqual){
+            if(operations.getText() == "+") {
+                result = Integer.parseInt(resultTextView.getText().toString()) + result;
+            }
+            else if (operations.getText() == "-") {
+                    result =   result - Integer.parseInt(resultTextView.getText().toString());
+             }
+            else if (operations.getText() == "*") {
+                result =   result * Integer.parseInt(resultTextView.getText().toString());
+            }
+            else if (operations.getText() == "/") {
+                if(Integer.parseInt(resultTextView.getText().toString()) != 0) {
+                    result = result / Integer.parseInt(resultTextView.getText().toString());
+                }
+                else resultTextView.setText("Деление на ноль невозможно");
+            }
+
+
+                resultTextView.setText(String.valueOf(result));
+                latestNumber.setText(resultTextView.getText());
+                result = 0;
+                equalPush = true;
+            }
+
+
+  }
+  public void clear(View view){
+        resultTextView.setText("");
+        operations.setText("");
+        latestNumber.setText("");
+  }
 
 }
